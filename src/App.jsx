23e6f2e6667495300195ea355e91955b71f0cc23
Daniel Seip <d_seip@mail.fhsu.edu>
2025-03-26 12:39:42 -0500

    Delete added, ready for midterm

--- src/App.jsx
+++ src/App.jsx
@@ -7,16 +7,19 @@ import Greeting from './Greeting'
 import TaskComponent from './TaskComponent'
 import Counter from './Counter'
 import TaskForm from './TaskForm'
+import TaskEntryDisplay from './TaskEntryDisplay'
+
+const taskArray = [{ name: 'Feed dogs', description: 'Get the dogs some food' },
+  { name: 'Feed cats', description: 'Get the cats some food' },
+  { name: 'Do homework', description: 'Spend some time getting schoolwork done' },
+  { name: 'Do work', description: 'Get work done for work' },
+  { name: 'Clean house', description: 'Do chores around the house' }];
 
 function App() {
   const [count, setCount] = useState(0)
 
-  const taskArray = [{name: 'Feed dogs', description: 'Get the dogs some food'},
-                    {name: 'Feed cats', description: 'Get the cats some food'},
-                    {name:'Do homework', description: 'Spend some time getting schoolwork done'},
-                    {name: 'Do work', description: 'Get work done for work'},
-                    {name: 'Clean house', description: 'Do chores around the house'}];
-  const [tasksWithId, setTasksWithId] = useState(taskArray.map((task, index) => ({name: task.name, description: task.description, id: crypto.randomUUID()}))); 
+
+  const [tasksWithId, setTasksWithId] = useState(taskArray.map((task, index) => ({ name: task.name, description: task.description, key: crypto.randomUUID() })));
 
 
   const handleAlert = () => {
@@ -24,17 +27,22 @@ function App() {
   };
 
   const handleSubmit = (name, description) => {
-    const newTasksWithId = [...tasksWithId, {name, description, id: crypto.randomUUID()}];
+    const newTasksWithId = [...tasksWithId, { name, description, key: crypto.randomUUID() }];
     setTasksWithId(newTasksWithId);
+  }
+
+  const handleDelete = (id) => {
+    const newTasks = tasksWithId.filter((task) => task.key !== id);
+    setTasksWithId(newTasks);
   }
 
   return (
     <div>
-      <Greeting username="Bob"/>
+      <Greeting username="Bob" />
       <hr />
       <Greeting username="Alice" />
       <hr />
-      <UserInfo handleClick={handleAlert}/>
+      <UserInfo handleClick={handleAlert} />
       <hr />
       <TaskComponent />
       <hr />
@@ -44,9 +52,13 @@ function App() {
       <br />
       <ul>
         {tasksWithId.map(task => (
-          <li key={task.id}>
-            {task.name} :: {task.description}
-          </li>
+          <TaskEntryDisplay
+            key={task.key}
+            taskName={task.name}
+            id={task.key}
+            description={task.description}
+            handleDelete={handleDelete}
+          />
         ))}
       </ul>
     </div>
---
+++ src/TaskEntryDisplay.jsx
@@ -0,0 +1,13 @@
+function TaskEntryDisplay({ id, taskName, description, handleDelete }) {
+    const handleClick = () => {
+      handleDelete(id);
+    };
+  
+    return (
+      <li>
+        {taskName} :: {description} <button onClick={handleClick}>Delete!</button>
+      </li>
+    );
+  }
+  
+  export default TaskEntryDisplay;
