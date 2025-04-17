38bc19ec888932b381e8d91909cf501cf2e3a4b7
Daniel Seip <d_seip@mail.fhsu.edu>
2025-04-17 08:18:22 -0500

    Adding firebase

--- src/BasicComponents.jsx
+++ src/BasicComponents.jsx
@@ -6,6 +6,7 @@ import Counter from "./Counter";
 
 
 
+
 function BasicComponents() {
 
     const [count, setCount] = useState(0);
---
+++ src/Firebase.js
@@ -0,0 +1,8 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    
+}
